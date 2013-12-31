012b1aab96a2089bc071b761f8ec4fc611640b62
wilkinsonbrian <wilkibrian@gmail.com>
2013-12-31 16:19:09 -0500

    Add Source Files

---
+++ Dorm Seating/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Dorm_Seating
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.fileBox = new System.Windows.Forms.TextBox();
+            this.saveText = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.loadStudents = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.loadStaff = new System.Windows.Forms.Button();
+            this.createSeating = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel3, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(560, 337);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Controls.Add(this.fileBox);
+            this.flowLayoutPanel3.Controls.Add(this.saveText);
+            this.flowLayoutPanel3.Controls.Add(this.button1);
+            this.flowLayoutPanel3.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(227, 3);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.tableLayoutPanel1.SetRowSpan(this.flowLayoutPanel3, 2);
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(330, 331);
+            this.flowLayoutPanel3.TabIndex = 3;
+            // 
+            // fileBox
+            // 
+            this.fileBox.Location = new System.Drawing.Point(3, 3);
+            this.fileBox.Multiline = true;
+            this.fileBox.Name = "fileBox";
+            this.fileBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.fileBox.Size = new System.Drawing.Size(327, 265);
+            this.fileBox.TabIndex = 0;
+            // 
+            // saveText
+            // 
+            this.saveText.AutoSize = true;
+            this.saveText.Location = new System.Drawing.Point(3, 274);
+            this.saveText.Name = "saveText";
+            this.saveText.Size = new System.Drawing.Size(87, 23);
+            this.saveText.TabIndex = 1;
+            this.saveText.Text = "Save Changes";
+            this.saveText.UseVisualStyleBackColor = true;
+            this.saveText.Click += new System.EventHandler(this.saveText_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 303);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 171);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(218, 163);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // loadStudents
+            // 
+            this.loadStudents.AutoSize = true;
+            this.loadStudents.Location = new System.Drawing.Point(3, 3);
+            this.loadStudents.Name = "loadStudents";
+            this.loadStudents.Size = new System.Drawing.Size(112, 23);
+            this.loadStudents.TabIndex = 0;
+            this.loadStudents.Text = "Choose Student File";
+            this.loadStudents.UseVisualStyleBackColor = true;
+            this.loadStudents.Click += new System.EventHandler(this.loadStudentFile);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.loadStudents);
+            this.flowLayoutPanel2.Controls.Add(this.loadStaff);
+            this.flowLayoutPanel2.Controls.Add(this.createSeating);
+            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(12, 29);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(218, 133);
+            this.flowLayoutPanel2.TabIndex = 2;
+            // 
+            // loadStaff
+            // 
+            this.loadStaff.AutoSize = true;
+            this.loadStaff.Location = new System.Drawing.Point(3, 32);
+            this.loadStaff.Name = "loadStaff";
+            this.loadStaff.Size = new System.Drawing.Size(112, 23);
+            this.loadStaff.TabIndex = 0;
+            this.loadStaff.Text = "Choose Staff File";
+            this.loadStaff.UseVisualStyleBackColor = true;
+            this.loadStaff.Click += new System.EventHandler(this.loadStaff_Click);
+            // 
+            // createSeating
+            // 
+            this.createSeating.AutoSize = true;
+            this.createSeating.Location = new System.Drawing.Point(3, 61);
+            this.createSeating.Name = "createSeating";
+            this.createSeating.Size = new System.Drawing.Size(112, 23);
+            this.createSeating.TabIndex = 1;
+            this.createSeating.Text = "Create Seating";
+            this.createSeating.UseVisualStyleBackColor = true;
+            this.createSeating.Click += new System.EventHandler(this.createSeating_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "\"Text files|*.txt|All files|*.*\"";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "\"Text files|*.txt|Word file|*.doc|All files|*.*\"";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 362);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Dorm Seating";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button loadStudents;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button loadStaff;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.TextBox fileBox;
+        private System.Windows.Forms.Button saveText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button createSeating;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Dorm Seating/Form1.cs
@@ -0,0 +1,411 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dorm_Seating
+{
+    public partial class Form1 : Form
+    {
+        private string staffFileName;
+        private string studentFileName;
+        private string saveFileName;
+        ProgressBar pBar1 = new ProgressBar();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Disables the ability to load the staff file and create the seating chart.
+            // User will load students, then staff, then create seating chart.
+            createSeating.Enabled = false;
+            loadStaff.Enabled = false;
+            pBar1.Location = new Point(3, 90);
+            flowLayoutPanel2.Controls.Add(pBar1);
+        }
+
+        /// <summary>
+        /// Prompts the user for a file containing student information.  Currently
+        /// each line of the file should consist of a students name followed by a comma and then
+        /// whether the student is (d)omestic or (i)nternational
+        /// 
+        /// e.g. Brian Wilkinson,d
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void loadStudentFile(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string studentList = File.ReadAllText(openFileDialog1.FileName);
+                fileBox.Text = studentList;
+                studentFileName = openFileDialog1.FileName;
+                loadStaff.Enabled = true; // once the student file has been loaded, enable staff file loading button
+            }
+        }
+
+        /// <summary>
+        /// Prompts the user for a text file containing staff information.
+        /// File consists of staff name followed by a comma and then the 
+        /// number of available seats at that table.
+        /// 
+        /// e.g. Brian,5
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void loadStaff_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string staffList = File.ReadAllText(openFileDialog1.FileName);
+                fileBox.Text = staffList;
+                staffFileName = openFileDialog1.FileName;
+                createSeating.Enabled = true; // Both files loaded, enable the ability to create the chart
+            }
+        }
+
+        /// <summary>
+        /// Clears whatever happens to be in the text box.
+        /// Not sure this is needed, may be removed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fileBox.Clear();
+        }
+
+        /// <summary>
+        /// User can make changes to whatever file is currently in the text box.
+        /// They will need to overwrite whichever file they are modifying and NOT
+        /// save a new one.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void saveText_Click(object sender, EventArgs e)
+        {
+            string toBeSaved = fileBox.Text;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, toBeSaved);
+            }
+            
+        }
+
+        /// <summary>
+        /// The main part of the program.  Only available after the student and staff files
+        /// have been selected.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void createSeating_Click(object sender, EventArgs e)
+        {
+            const int NUM_WEEKS = 8;  // More than 8 weeks and the recursion runs into trouble.
+
+            List<Student> students = new List<Student>();
+            List<Table> tables = new List<Table>();
+            GetStudents(students, studentFileName);
+            GetTables(tables, staffFileName);
+            int totalSeats = GetTotalSeats(tables);
+
+            // Configure the progress bar.
+            pBar1.Minimum = 1;
+            pBar1.Maximum = NUM_WEEKS;
+            pBar1.Step = 1;
+            pBar1.Visible = true;
+
+            // Check to make sure that the number of available seats is 
+            // the same as the number of students.
+            if (totalSeats == students.Count)
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    saveFileName = saveFileDialog1.FileName;
+                }
+                MakeSeatingChart(NUM_WEEKS, students, tables, saveFileName, pBar1);
+            }
+            else
+                MessageBox.Show("Number of students does not equal number of seats", "Error");
+        }
+
+        /// <summary>
+        /// Reads the students from a text file and adds them to a List. Students are 
+        /// added as a Student object.  Country information must be separated by a comma.
+        /// </summary>
+        /// <param name="students"></param>
+        /// <param name="studentFileName"></param>
+        private static void GetStudents(List<Student> students, string studentFileName)
+        {
+            using (StreamReader sr = new StreamReader(studentFileName))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] sRecord = line.Split(',');
+                    Student student = new Student(sRecord[0], sRecord[1]);
+                    students.Add(student);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Reads the staff and available seating information from a text file.  Staff name
+        /// and available seats must be separated by a comma.  Tables are added as a Table object.
+        /// </summary>
+        /// <param name="tables"></param>
+        /// <param name="staffFileName"></param>
+        private static void GetTables(List<Table> tables, string staffFileName)
+        {
+            using (StreamReader sr = new StreamReader(staffFileName))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] tRecord = line.Split(',');
+                    Table newTable = new Table(tRecord[0], Convert.ToInt32(tRecord[1]));
+                    tables.Add(newTable);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Used to find the total number of seats available.  This number must match
+        /// the total number of students.  If not, the number of seats must be changed
+        /// at one or more tables.
+        /// </summary>
+        /// <param name="tables"></param>
+        /// <returns></returns>
+        private static int GetTotalSeats(List<Table> tables)
+        {
+            int total = 0;
+            foreach (Table table in tables)
+            {
+                total += table.GetNumSeats();
+            }
+            return total;
+        }
+
+
+        private static void MakeSeatingChart(int NUM_WEEKS, List<Student> students, List<Table> tables, string saveFileName, ProgressBar pBar1)
+        {            
+            for (int j = 0; j < NUM_WEEKS; j++)
+            {
+                ResetWeek(tables, students);
+                int NumStudentsSeated = 0;
+                SeatRecursive(students, tables, NumStudentsSeated);
+                pBar1.PerformStep();
+                foreach (Table table in tables)
+                {
+                    File.AppendAllText(saveFileName, table.GetStaffName());
+                    File.AppendAllText(saveFileName, "\n");
+                    foreach (Student student in table.GetTableSeating())
+                    {
+                        File.AppendAllText(saveFileName, student.getName());
+                        File.AppendAllText(saveFileName, ",");
+                    }
+                    File.AppendAllText(saveFileName, "\n");
+                    File.AppendAllText(saveFileName, "\n");
+                }
+
+                File.AppendAllText(saveFileName, "\f");
+            }
+
+            MessageBox.Show("Seating chart has been created", "Done");
+        }
+
+        /// <summary>
+        /// Clears past week's information so that students can be seated at different tables.
+        /// </summary>
+        /// <param name="tables"></param>
+        /// <param name="students"></param>
+        private static void ResetWeek(List<Table> tables, List<Student> students)
+        {
+            foreach (Table table in tables)
+            {
+                table.ClearStudents();
+            }
+
+            foreach (Student student in students)
+            {
+                student.UnseatStudent();
+            }
+        }
+
+        /// <summary>
+        /// Recursive function that actually seats all the students at tables, one week at a time.  Base case
+        /// occurs when the number of students who have been seated is the same as the total number of students.
+        /// The algorithm works by first selecting a random student.  It then starts at the first table and checks to
+        /// see if a) there is space for the student and b) the student has not sat at that table during a previous week.
+        /// If both are true, the student is added to the list for that table and is then marked as "seated."
+        /// If no tables meet that condition, the previous student that had been seated is removed 
+        /// and seated at another table.
+        /// </summary>
+        /// <param name="students"></param>
+        /// <param name="tables"></param>
+        /// <param name="NumStudentsSeated"></param>
+        /// <returns></returns>
+        private static bool SeatRecursive(List<Student> students, List<Table> tables, int NumStudentsSeated)
+        {
+            if (NumStudentsSeated == students.Count()) return true; //base case
+
+            int NextStudent = RandomSelect(students);
+            for (int i = 0; i < tables.Count; i++)
+            {
+                if (tables[i].GetRemainingSeats() > 0 && !students[NextStudent].SatAtTable(i))
+                {
+                    AddStudentToTable(students, NextStudent, tables, i);
+                    students[NextStudent].SeatStudent(); // sets isSeated property to true
+                    NumStudentsSeated++;
+                    if (SeatRecursive(students, tables, NumStudentsSeated))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        RemoveStudentFromTable(students, NextStudent, tables, i);
+                        students[NextStudent].UnseatStudent();
+                        NumStudentsSeated--;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private static int RandomSelect(List<Student> students)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, students.Count);
+
+            // if a student has already been seated, pick a new number.
+            while (students[randomNumber].HasBeenSeated())
+            {
+                randomNumber = random.Next(0, students.Count);
+            }
+            return randomNumber;
+        }
+
+        private static void AddStudentToTable(List<Student> students, int NextStudent, List<Table> tables, int i)
+        {
+            tables[i].AddStudent(students[NextStudent]);
+            students[NextStudent].AddTable(i);
+        }
+
+        private static void RemoveStudentFromTable(List<Student> students, int NextStudent, List<Table> tables, int i)
+        {
+            tables[i].RemoveStudent(students[NextStudent]);
+            students[NextStudent].RemoveTable(i);
+        }
+
+    }
+
+    class Table
+    {
+        private string Staff;
+        private int NumSeats;
+        private List<Student> Seating = new List<Student>();
+
+        public Table(string staff, int numSeats)
+        {
+            this.Staff = staff;
+            this.NumSeats = numSeats;
+        }
+
+        public string GetStaffName()
+        {
+            return Staff;
+        }
+
+        public int GetNumSeats()
+        {
+            return NumSeats;
+        }
+
+        public void AddStudent(Student student)
+        {
+            Seating.Add(student);
+        }
+
+        public void RemoveStudent(Student student)
+        {
+            Seating.Remove(student);
+        }
+
+        public void ClearStudents()
+        {
+            Seating.Clear();
+        }
+        public List<Student> GetTableSeating()
+        {
+            return Seating;
+        }
+
+        public int GetRemainingSeats()
+        {
+            return NumSeats - Seating.Count;
+        }
+    }
+
+    class Student
+    {
+        private string Name;
+        private string Country;
+        private bool IsSeated = false;
+        private List<int> TablesSatAt = new List<int>();
+
+        public Student(string name, string country)
+        {
+            this.Name = name;
+            this.Country = country;
+        }
+
+        public string getName()
+        {
+            return Name;
+        }
+
+        public string getCountry()
+        {
+            return Country;
+        }
+
+        public void AddTable(int tableNumber)
+        {
+            TablesSatAt.Add(tableNumber);
+        }
+
+        public void RemoveTable(int tableNumber)
+        {
+            TablesSatAt.Remove(tableNumber);
+        }
+
+        public bool HasBeenSeated()
+        {
+            return IsSeated;
+        }
+
+        public void SeatStudent()
+        {
+            IsSeated = true;
+        }
+
+        public void UnseatStudent()
+        {
+            IsSeated = false;
+        }
+
+        public bool SatAtTable(int tableNumber)
+        {
+            foreach (int seat in TablesSatAt)
+            {
+                if (seat == tableNumber) return true;
+            }
+            return false;
+        }
+    }
+}
